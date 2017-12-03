a227cd9aeed3d922ca6c780b0209aa34ecd53d4c
Michael Pellegrini <mipelleg@microsoft.com>
2017-12-03 15:14:02 -0800

    Moves lessons views to a partial

---
+++ Models/LessonsView.cs
@@ -0,0 +1,9 @@
+namespace BlackMoonStudio.Models
+{
+    public class LessonsView
+    {
+        public string Title { get; set; }
+        public string Path { get; set; }
+        public Lesson[] Lessons { get; set; }
+    }
+}
