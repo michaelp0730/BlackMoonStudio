6e3e1e6dcd761b0151d2a172b611197421b87ffd
Michael Pellegrini <mipelleg@microsoft.com>
2017-12-06 15:38:33 -0800

    Supports routing for all lesson categories

--- Controllers/LessonsController.cs
+++ Controllers/LessonsController.cs
@@ -11,25 +11,62 @@ namespace BlackMoonStudio.Controllers
 {
     public class LessonsController : Controller
     {
-        [HttpGet("lessons/beginner/{slug?}/")]
-        public IActionResult GetBeginnerLesson(string slug)
+        [HttpGet("lessons/{category}/{slug?}/")]
+        public IActionResult GetLessons(string category, string slug)
         {
             var lesson = new Lesson();
-            var beginnerLessons = lesson.GetLessons("Beginner");
-            if (!string.IsNullOrEmpty(slug))
-            {
-                lesson = beginnerLessons.FirstOrDefault(x => x.Slug == slug);
-                return View("Pages/lessons/_details.cshtml", lesson);
-            }
-
             var lessonsCuration = lesson.GetCurationList("Lessons");
-            var beginnerCuration = lessonsCuration.FirstOrDefault(x => x.Slug == "Beginner");
-            return View("Pages/lessons/_landing.cshtml", new LessonCategoryIndex
+            switch (category)
             {
-                Heading = "Beginner Guitar Lessons",
-                Lessons = beginnerLessons.OrderBy(x => 
-                    { return Array.IndexOf(beginnerCuration.LessonSlugs, x.Slug); } ).ToArray(),
-            });
+                case "advanced":
+                    var advancedLessons = lesson.GetLessons("Advanced");
+                    if (!string.IsNullOrEmpty(slug))
+                    {
+                        lesson = advancedLessons.FirstOrDefault(x => x.Slug == slug);
+                        return View("Pages/lessons/_details.cshtml", lesson);
+                    }
+
+                    var advancedCuration = lessonsCuration.FirstOrDefault(x => x.Slug == "Advanced");
+                    return View("Pages/lessons/_landing.cshtml", new LessonCategoryIndex
+                    {
+                        Heading = "Advanced Guitar Lessons",
+                        Lessons = advancedLessons.OrderBy(x => 
+                            { return Array.IndexOf(advancedCuration.LessonSlugs, x.Slug); } ).ToArray(),
+                    });
+                    break;
+                case "intermediate":
+                    var intermediateLessons = lesson.GetLessons("Intermediate");
+                    if (!string.IsNullOrEmpty(slug))
+                    {
+                        lesson = intermediateLessons.FirstOrDefault(x => x.Slug == slug);
+                        return View("Pages/lessons/_details.cshtml", lesson);
+                    }
+
+                    var intermediateCuration = lessonsCuration.FirstOrDefault(x => x.Slug == "Intermediate");
+                    return View("Pages/lessons/_landing.cshtml", new LessonCategoryIndex
+                    {
+                        Heading = "Intermediate Guitar Lessons",
+                        Lessons = intermediateLessons.OrderBy(x => 
+                            { return Array.IndexOf(intermediateCuration.LessonSlugs, x.Slug); } ).ToArray(),
+                    });
+                    break;
+                default:
+                    var beginnerLessons = lesson.GetLessons("Beginner");
+                    if (!string.IsNullOrEmpty(slug))
+                    {
+                        lesson = beginnerLessons.FirstOrDefault(x => x.Slug == slug);
+                        return View("Pages/lessons/_details.cshtml", lesson);
+                    }
+
+                    var beginnerCuration = lessonsCuration.FirstOrDefault(x => x.Slug == "Beginner");
+                    return View("Pages/lessons/_landing.cshtml", new LessonCategoryIndex
+                    {
+                        Heading = "Beginner Guitar Lessons",
+                        Lessons = beginnerLessons.OrderBy(x => 
+                            { return Array.IndexOf(beginnerCuration.LessonSlugs, x.Slug); } ).ToArray(),
+                    });
+                    break;
+            }
         }
     }
 }
