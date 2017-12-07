aad46e2776f8776523ec1f494868ce1718433b6b
Michael Pellegrini <mipelleg@microsoft.com>
2017-12-06 23:01:21 -0800

    Fixes path in links from category index pages

--- Controllers/LessonsController.cs
+++ Controllers/LessonsController.cs
@@ -31,8 +31,10 @@ namespace BlackMoonStudio.Controllers
                     curation = lessonsCuration.FirstOrDefault(x => x.Slug == "Advanced");
                     return View("Pages/lessons/_landing.cshtml", new LessonCategoryIndex
                     {
+                        Level = Levels.Advanced,
                         Heading = "Advanced Guitar Lessons",
                         Summary = "Time to take your game to a whole new level. The advanced lessons are more focused on techniques than anything else. Be prepared to practice - a lot - as these techniques require significant muscle memory, and rhythm. As always, you'll be better off if you practice with a metronome.",
+                        Path = "/lessons/advanced/",
                         Lessons = lessonList.OrderBy(x => 
                             { return Array.IndexOf(curation.LessonSlugs, x.Slug); } ).ToArray(),
                     });
@@ -47,8 +49,10 @@ namespace BlackMoonStudio.Controllers
                     curation = lessonsCuration.FirstOrDefault(x => x.Slug == "Intermediate");
                     return View("Pages/lessons/_landing.cshtml", new LessonCategoryIndex
                     {
+                        Level = Levels.Intermediate,
                         Heading = "Intermediate Guitar Lessons",
                         Summary = "Time to take the basic concepts from the Beginner category and start learning lots of fun new stuff. Soon you will feel comfortable playing leads and solos in any key, anywhere on the neck. Your understanding of music theory, and your comfort level navigating the fretboard, are about to grow significantly. Let's get started!",
+                        Path = "/lessons/intermediate/",
                         Lessons = lessonList.OrderBy(x => 
                             { return Array.IndexOf(curation.LessonSlugs, x.Slug); } ).ToArray(),
                     });
@@ -63,8 +67,10 @@ namespace BlackMoonStudio.Controllers
                     curation = lessonsCuration.FirstOrDefault(x => x.Slug == "Beginner");
                     return View("Pages/lessons/_landing.cshtml", new LessonCategoryIndex
                     {
+                        Level = Levels.Beginner,
                         Heading = "Beginner Guitar Lessons",
                         Summary = "These lessons will take the absolute beginner to a point where they are ready to start exploring various playing techniques, and genre-specific licks. Like all categories, the lessons are broken down into 3 stages, so you can think of the stages as mini categories.",
+                        Path = "/lessons/beginner/",
                         Lessons = lessonList.OrderBy(x => 
                             { return Array.IndexOf(curation.LessonSlugs, x.Slug); } ).ToArray(),
                     });
--- Models/LessonCategoryIndex.cs
+++ Models/LessonCategoryIndex.cs
@@ -2,8 +2,10 @@ namespace BlackMoonStudio.Models
 {
     public class LessonCategoryIndex
     {
+        public Levels Level { get; set; }
         public string Heading { get; set; }
         public string Summary { get; set; }
+        public string Path { get; set; }
         public Lesson[] Lessons { get; set; }
     }
 }
