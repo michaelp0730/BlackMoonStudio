f403bb4cb71bc09fd7c40894c4f2ff1605e831d9
Michael Pellegrini <mipelleg@microsoft.com>
2017-12-09 10:55:10 -0800

    Adds multiple videos to welcome lesson

--- Models/Video.cs
+++ Models/Video.cs
@@ -7,6 +7,6 @@ namespace BlackMoonStudio.Models
     {
         public string Slug { get; set; }
         public string Title { get; set; }
-        public HtmlString Caption { get; set; }
+        public string Caption { get; set; }
     }
 }
